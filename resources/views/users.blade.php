d3d976975c9fc398573c0d91b2d41132bef57847
Oleg Dolya <oleg.dolya2706@gmail.com>
2025-02-26 21:16:29 +0300

    Task3.16.

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -12,26 +12,26 @@
     </style>
 </head>
 <body>
-        <h1>Список пользователей</h1>
-        <table>
-            <th>
-                <tr>
-                    <th>#</th>
-                    <th>Имя</th>
-                    <th>Фамилия</th>
-                    <th>Статус</th>
-                </tr>
-            </th>
-            <tbody>
-                @foreach($users as $index => $user)
-                    <tr class="{{ $user['banned'] ? 'banned' : 'active' }}">
-                        <td>{{ $index + 1 }}</td>
-                        <td>{{ $user['name'] }}</td>
-                        <td>{{ $user['surname'] }}</td>
-                        <td>{{ $user['banned'] ? 'забанен' : 'активен' }}</td>
+    <h1>Список пользователей</h1>
+    <table>
+        <th>
+            <tr>
+                <th>#</th>
+                <th>Имя</th>
+                <th>Фамилия</th>
+                <th>Статус</th>
+            </tr>
+        </th>
+        <tbody>
+            @foreach($users as $index => $user)
+                <tr class="{{ $user['banned'] ? 'banned' : 'active' }}">
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $user['name'] }}</td>
+                    <td>{{ $user['surname'] }}</td>
+                    <td>{{ $user['banned'] ? 'забанен' : 'активен' }}</td>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
